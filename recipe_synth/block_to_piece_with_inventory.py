9661dd8fef80ea5915f4cf32cfee4af07078dda5
Alessandro Giugno <alessandro.giugno@gmx.de>
2024-05-02 15:37:52 +0200

    chore(recipe-synth): parameterized goal dimension

--- recipe_synth/block_to_piece_with_inventory.py
+++ recipe_synth/block_to_piece_with_inventory.py
@@ -57,7 +57,7 @@ inc.add_precondition(Equals(type2id(c), type2id(blk)))
 dec.add_decrease_effect(piece_dim(c), block_size(blk))
 dec.add_increase_effect(block_dim(blk), 1)
 
-problem = Problem('CounterWithInventory')
+problem = Problem('BlockToPieceWithInventory')
 
 problem.add_fluent(piece_dim, default_initial_value=0)
 problem.add_fluent(block_dim, default_initial_value=0)
@@ -67,7 +67,7 @@ problem.add_fluent(type2id, default_initial_value=0)
 domain_objects_names = []
 domain_objects = {}
 
-NOBJ = 3
+NOBJ = 2
 TERMGOAL = "piece_dim"
 TERMELEM = "fabric_block_dim"
 for id in range(NOBJ):
@@ -81,24 +81,41 @@ for dobjn in domain_objects_names:
 problem.add_objects(domain_objects.values())
 
 for obj_id in range(NOBJ):
-    problem.set_initial_value(block_size(domain_objects[f"{TERMELEM}{obj_id}"]), obj_id)
+    problem.set_initial_value(block_size(domain_objects[f"{TERMELEM}{obj_id}"]), 1 + obj_id)
     problem.set_initial_value(type2id(domain_objects[f"{TERMELEM}{obj_id}"]), obj_id)
     problem.set_initial_value(type2id(domain_objects[f"{TERMGOAL}{obj_id}"]), obj_id)
 # Not looped since they shall have different values
 problem.set_initial_value(block_dim(domain_objects[f"{TERMELEM}0"]), 5) 
 problem.set_initial_value(block_dim(domain_objects[f"{TERMELEM}1"]), 4) 
-problem.set_initial_value(block_dim(domain_objects[f"{TERMELEM}2"]), 3) 
 
 problem.add_action(inc)
 problem.add_action(dec)
 
-problem.add_goal(GE(
-    Plus(
-        Plus(piece_dim(domain_objects[f"{TERMGOAL}0"]), piece_dim(domain_objects[f"{TERMGOAL}1"])),
-        piece_dim(domain_objects[f"{TERMGOAL}2"])), 10)
+GOALSIZE=6
+if 3 == NOBJ:
+    problem.set_initial_value(block_dim(domain_objects[f"{TERMELEM}2"]), 3)
+    problem.add_goal(
+        LE(
+            GOALSIZE,
+            Plus(
+                Plus(
+                    piece_dim(domain_objects[f"{TERMGOAL}0"]),
+                    piece_dim(domain_objects[f"{TERMGOAL}1"])
+                ),
+                piece_dim(domain_objects[f"{TERMGOAL}2"]))
+        )
     )
-print(problem)
+elif 2 == NOBJ:
+    problem.add_goal(
+        LE(
+            GOALSIZE,
+            Plus(
+                piece_dim(domain_objects[f"{TERMGOAL}0"]),
+                piece_dim(domain_objects[f"{TERMGOAL}1"])),
+        )
+    )
 
+print(problem)
 to_pddl.export(problem, plibPath("benchmarks") / os.path.basename(__file__).strip(".py"))
 
 with OneshotPlanner(name='enhsp') as planner:
