e4f5b3148364c70a17aae08da1b0b21286cedfd8
Alessandro Giugno <alessandro.giugno@gmx.de>
2024-05-22 11:59:52 +0200

    chore(app): explicit (dependency) is better than implicit

--- recipe_synth/app/callbacks.py
+++ recipe_synth/app/callbacks.py
@@ -1,6 +1,7 @@
 from dash import html, dcc
 from dash.dependencies import Input, Output
 from unified_planning.shortcuts import SequentialSimulator
+from pandas import DataFrame
 
 from recipe_synth import block_to_piece, plan_goal_simulator
 
@@ -27,7 +28,7 @@ def get_callbacks(app):
         problem, plan = block_to_piece.run_planner(should_run_simulation=False)
         if plan:
             with SequentialSimulator(problem=problem) as sim:
-                df = plan_goal_simulator.simulate_b2p(plan, sim, problem)
+                df: DataFrame = plan_goal_simulator.simulate_b2p(plan, sim, problem)
         
         set_progress(["Sequential plan:"])
         return [html.Div([dcc.Markdown(children=df.to_markdown())])]
